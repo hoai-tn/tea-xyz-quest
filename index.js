c7ab4d9e94e6dfb98cc42ee8e6aa58aad1557c92
Hoai Tran <tranngochoai02051999@gmail.com>
2024-04-21 09:13:32 +0700

    update package

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-module.exports = function() {
-  console.log('tea.xyz');
-  return;
-};
+function parseDatetime(datetimeString) {
+  const parsedDate = new Date(datetimeString);
+  if (isNaN(parsedDate.getTime())) {
+    throw new Error('Invalid datetime string');
+  }
+  return parsedDate;
+}
+
+module.exports = parseDatetime;
